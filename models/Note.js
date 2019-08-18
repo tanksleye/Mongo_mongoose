f5a25235cb6e20a07d1c889b794f2ee703f88b71
Jet <elesebatanksley@yahoo.com>
2019-08-18 17:50:07 -0500

    tried to get it, but it keeps coming up with errors

---
+++ models/Note.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var noteSchema = new Schema({
+  
+  _headlineId: {
+    type: Schema.Types.ObjectId,
+    ref: "Headline"
+  },
+
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  
+  noteText: String
+});
+
+
+var Note = mongoose.model("Note", noteSchema);
+
+
+module.exports = Note;
